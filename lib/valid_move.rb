422764f29777dab97350f13fc0dd1163e91569ac
Logan Stone <logan.stone20@gmail.com>
2019-08-02 00:38:30 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if (!position_taken?(board, index))
+  if 0 <= index <= 8
+    nil
+  end
+  elsif (!position_taken?(board, index))
     true
   elsif position_taken?(board, index)
     false
